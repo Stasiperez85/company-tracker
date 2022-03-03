1e091932d0e1be2b7e68dd8feaa6242f2bf99a16
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-03 15:10:26 -0700

    create role table

--- db/schema.sql
+++ db/schema.sql
@@ -8,12 +8,11 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
--- CREATE TABLE role (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   title VARCHAR(30) NOT NULL,
---   salary DECIMAL(7,2) NOT NULL,
---   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
--- );
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(7,2) NOT NULL
+);
 
 -- CREATE TABLE employee (
 --   id INTEGER AUTO_INCREMENT PRIMARY KEY,
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,11 +4,11 @@ VALUES
   ('Sales Rep'),
   ('Implemenation');
 
---   INSERT INTO role (title, salary, department_id)
--- VALUES
---   ('Payroll', '71000.00', 1),
---   ('Senior', '85000.00',2),
---   ('IM', '62000.00', 3);
+  INSERT INTO role (title, salary)
+VALUES
+  ('Payroll', '71000.00'),
+  ('Senior', '85000.00'),
+  ('IM', '62000.00');
 
 -- INSERT INTO employee
 --   (first_name, last_name, role_id,)
