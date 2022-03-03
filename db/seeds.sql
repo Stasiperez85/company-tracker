b2e383be2332f087b78170062ba856a471c7d4ab
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-03 12:03:51 -0700

    save code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO department (name)
-VALUES
-  ('HR'),
-  ('Sales Rep'),
-  ('Implemenation');
+-- INSERT INTO department (name)
+-- VALUES
+--   ('HR'),
+--   ('Sales Rep'),
+--   ('Implemenation');
 
-  INSERT INTO role (title, salary, department_id)
-VALUES
-  ('Payroll', '71000.00', 1),
-  ('Senior', '85000.00',2),
-  ('IM', '62000.00', 3);
+--   INSERT INTO role (title, salary, department_id)
+-- VALUES
+--   ('Payroll', '71000.00', 1),
+--   ('Senior', '85000.00',2),
+--   ('IM', '62000.00', 3);
