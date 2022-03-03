4647301a61090eb93143e6e4643fb5b2eab484ee
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-03 13:05:36 -0700

    install node and express

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,8 +4,16 @@ VALUES
   ('Sales Rep'),
   ('Implemenation');
 
-  INSERT INTO role (title, salary, department_id)
-VALUES
-  ('Payroll', '71000.00', 1),
-  ('Senior', '85000.00',2),
-  ('IM', '62000.00', 3);
+--   INSERT INTO role (title, salary, department_id)
+-- VALUES
+--   ('Payroll', '71000.00', 1),
+--   ('Senior', '85000.00',2),
+--   ('IM', '62000.00', 3);
+
+-- INSERT INTO employee
+--   (first_name, last_name, role_id,)
+-- VALUES
+--   ('Hermonie', 'Granger', 1),
+--   ('Ron', 'Weasley', 1),
+--   ('Draco', 'Malfoy', 1),
+--   ('Harry', 'Potter', 2),
