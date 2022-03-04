45fbd8d530f15a0704767ec426ee5a1bd75e1066
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-04 15:18:03 -0700

    add inquirer prompts and insert data into tables

--- db/schema.sql
+++ db/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE SET NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role (id),
   manager_id INTEGER,
  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -18,9 +18,12 @@ const promptCompany = async () => {
                     "View Department",
                     "View Roles",
                     "View Employees",
+                    "Add Department",
+                    "Add Role",
+                    "Add Employee",
+                    "Update an employee role",
                 ]
             }])
-
 
     switch (answers.options) {
         case 'View Department':
@@ -29,12 +32,120 @@ const promptCompany = async () => {
         case 'View Roles':
             viewRoles();
             break;
+        case 'View Employees':
+            viewEmployees();
+            break;
+
+        case 'Add Department':
+            addDepartment();
+            break;
+        case 'Add Role':
+            addRole();
+            break;
+        case 'Add Employee':
+            addEmployee();
+            break;
         default:
+            quit();
             console.log("Hello");
     }
 
 };
 
+promptCompany();
+
+const viewDepartments = () => {
+    db.query(`SELECT * FROM department`, (err, rows) => {
+        if (err) throw err
+        console.table(rows);
+    });
+}
+
+const viewRoles = () => {
+    db.query(`SELECT * FROM role`, (err, rows) => {
+        if (err) throw err
+        console.table(rows);
+    });
+}
+
+const viewEmployees = () => {
+    db.query(`SELECT * FROM employee`, (err, rows) => {
+        if (err) throw err
+        console.table(rows);
+    });
+}
+
+const addDepartment = async () => {
+    const answers = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What Department would you like to add?"
+            }
+        ]);
+    db.query(`INSERT INTO department (name) VALUES (?)`, [answers.name], (err, rows) => {
+        if (err) throw err
+        console.table(rows);
+        promptCompany();
+    });
+};
+
+const addRole = async () => {
+    const answers = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: "What Role would you like to add?"
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: "What is the salary for the new Role"
+            },
+            {
+                type: 'input',
+                name: 'deptID',
+                message: "What is the Department ID?"
+            }
+        ]);
+    db.query(`INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`, [answers.title, answers.salary, answers.deptID], (err, rows) => {
+        if (err) throw err
+        console.table(rows);
+        promptCompany();
+    });
+};
+const addEmployee = async () => {
+    const answers = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: "What is the new employees first name?"
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: "What is the new employees last name?"
+            },
+            {
+                type: 'input',
+                name: 'roleID',
+                message: "What is the employee's ID?"
+            },
+            {
+                type: 'input',
+                name: 'managerID',
+                message: "What is the manager's ID?"
+            }
+        ]);
+    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`, [answers.first_name, answers.last_name, answers.roleID, answers.managerID], (err, rows) => {
+        if (err) throw err
+        console.table(rows);
+        promptCompany();
+    });
+};
 // const promptCompany = () => {
 //     inquirer
 //         .prompt([
@@ -61,26 +172,3 @@ const promptCompany = async () => {
 //                 }
 //             })
 // };
-
-promptCompany();
-
-const viewDepartments = () => {
-    db.query(`SELECT * FROM department`, (err, rows) => {
-        if (err) throw err
-        console.table(rows);
-    });
-}
-
-const viewRoles = () => {
-    db.query(`SELECT * FROM role`, (err, rows) => {
-        if (err) throw err
-        console.table(rows);
-    });
-}
-
-// const viewDepartments = () => {
-//     db.query(`SELECT * FROM department`, (err, rows) => {
-//         if (err) throw err
-//         console.table(rows);
-//     });
-// }
